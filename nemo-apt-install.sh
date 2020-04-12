8b07e01f40f163b792350edaff1a096c0c4975c6
Ramon Mesquita <ramonmesquita113@gmail.com>
2020-04-12 17:11:30 -0300

    Ajuste script nemo para não remover nautilus
    No Ubuntu Budgie as opções da área de trabalho depende do nautilus

--- nemo-apt-install.sh
+++ nemo-apt-install.sh
@@ -3,7 +3,3 @@ xdg-mime default nemo.desktop inode/directory application/x-gnome-saved-search
 gsettings set org.gnome.desktop.background show-desktop-icons false
 gsettings set org.nemo.desktop show-desktop-icons true
 xdg-mime query default inode/directory
-sudo apt-get remove nautilus
-sudo apt-get remove --auto-remove nautilus
-sudo apt-get purge nautilus
-sudo apt-get purge --auto-remove nautilus
