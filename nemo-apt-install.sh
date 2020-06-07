cfc14864afd0395f4dff91063984acfcff70eddf
Ramon Mesquita <ramonmesquita113@gmail.com>
2020-06-07 15:04:21 -0300

    Alteração apt-get para apt

--- nemo-apt-install.sh
+++ nemo-apt-install.sh
@@ -1,4 +1,4 @@
-sudo apt-get install nemo
+sudo apt install nemo
 xdg-mime default nemo.desktop inode/directory application/x-gnome-saved-search
 gsettings set org.gnome.desktop.background show-desktop-icons false
 gsettings set org.nemo.desktop show-desktop-icons true
