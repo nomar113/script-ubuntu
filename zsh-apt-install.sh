e94336c00fe727d4f60c161ea4b22a585af0483e
Ramon Mesquita <ramonmesquita113@gmail.com>
2020-07-09 15:33:00 -0300

    Update Zsh script with my .zshrc on GitHub gist

--- zsh-apt-install.sh
+++ zsh-apt-install.sh
@@ -1,32 +1,10 @@
 sudo apt install zsh curl wget -y
+wget https://gist.githubusercontent.com/nomar113/81e3ee01db161cb18d11b924648f7f45/raw/befddde78d5ae04366319f3e12eb0cee5342562d/.zshrc -P ~
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-wget https://github.com/dracula/tilix/blob/master/Dracula.json
+wget https://raw.githubusercontent.com/dracula/tilix/master/Dracula.json
 mkdir -p ~/.config/tilix/schemes
 mv Dracula.json ~/.config/tilix/schemes
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-'
-SPACESHIP_PROMPT_ORDER=(
-  user          # Username section
-  dir           # Current directory section
-  host          # Hostname section
-  git           # Git section (git_branch + git_status)
-  hg            # Mercurial section (hg_branch  + hg_status)
-  exec_time     # Execution time
-  line_sep      # Line break
-  vi_mode       # Vi-mode indicator
-  jobs          # Background jobs indicator
-  exit_code     # Exit code section
-  char          # Prompt character
-)
-SPACESHIP_USER_SHOW=always
-SPACESHIP_PROMPT_ADD_NEWLINE=false
-SPACESHIP_CHAR_SYMBOL="❯"
-SPACESHIP_CHAR_SUFFIX=" "
-' >> ~/.zshrc
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
-'
-zinit light zdharma/fast-syntax-highlighting
-zinit light zsh-users/zsh-autosuggestions
-zinit light zsh-users/zsh-completions
-' >> ~/.zshrc
+
