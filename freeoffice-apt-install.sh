572f1d564e0296d1d6fa454dacc3477d036ef336
Ramon Mesquita <ramonmesquita113@gmail.com>
2020-07-10 08:40:18 -0300

    Update Free Office script with '-y'

--- freeoffice-apt-install.sh
+++ freeoffice-apt-install.sh
@@ -1,2 +1,2 @@
 wget https://www.softmaker.net/down/softmaker-freeoffice-2018_976-01_amd64.deb -O freeoffice.deb
-sudo apt install ./freeoffice.deb #equals to dpkg -i package.deb && sudo apt install -f
+sudo apt install ./freeoffice.deb -y #equals to dpkg -i package.deb && sudo apt install -f
