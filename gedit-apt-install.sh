940e7752358ca2304900eba53ec6aa832a2d843f
Ramon Mesquita <ramonmesquita113@gmail.com>
2020-07-15 18:23:33 -0300

    Update the Gedit script to create the path to Dracula theme

--- gedit-apt-install.sh
+++ gedit-apt-install.sh
@@ -1,3 +1,4 @@
 sudo apt install gedit -y
+mkdir -p $HOME/.local/share/gedit/styles
 wget https://raw.githubusercontent.com/dracula/gedit/master/dracula.xml
 mv dracula.xml $HOME/.local/share/gedit/styles/
