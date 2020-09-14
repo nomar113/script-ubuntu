379b967fd4204f23c5087d951ce7b05083a4437b
Ramon Mesquita <ramonmesquita113@gmail.com>
2020-09-13 22:11:51 -0300

    Update script to remove default apps Ubuntu

--- gnome_apps-apt-remove.sh
+++ gnome_apps-apt-remove.sh
@@ -1,1 +1,1 @@
-sudo apt remove gnome-sudoku gnome-mines gnome-mahjongg gnome-todo gnome-todo-common aisleriot -y
+sudo apt remove gnome-sudoku gnome-mines gnome-mahjongg gnome-todo gnome-todo-common aisleriot shotwell shotwell-common -y
