722442f6daf07a16a2dca6cdf903017abbac78a7
Ramon Mesquita <ramon.mesquita@zoom.com.br>
2020-10-30 23:57:57 -0300

    Script to install Charles Proxy

---
+++ charles_proxy-apt-install.sh
@@ -0,0 +1,4 @@
+wget -q -O - https://www.charlesproxy.com/packages/apt/PublicKey | sudo apt-key add -
+sudo sh -c 'echo deb https://www.charlesproxy.com/packages/apt/ charles-proxy main > /etc/apt/sources.list.d/charles.list'
+sudo apt update
+sudo apt install charles-proxy -y
