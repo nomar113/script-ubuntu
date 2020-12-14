f8244ca0d364f189697f487d4eff6ed7fc732164
Ramon Mesquita <ramonmesquita113@gmail.com>
2020-12-14 08:53:17 -0300

    Script to install Virtual Box

---
+++ virtual_box-apt-install.sh
@@ -0,0 +1,5 @@
+sudo apt install wget -y
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+sudo apt update
+sudo apt install virtualbox virtualbox-ext-pack -y
