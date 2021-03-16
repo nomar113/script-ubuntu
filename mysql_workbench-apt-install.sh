2d0eccad541c72e7dd352281b4f5d28293247722
Ramon Mesquita <ramonmesquita113@gmail.com>
2021-03-15 21:22:53 -0300

    Script to install MySQL Workbench

---
+++ mysql_workbench-apt-install.sh
@@ -0,0 +1,5 @@
+wget -O mysql-apt-config.deb https://dev.mysql.com/get/mysql-apt-config_0.8.16-1_all.deb
+sudo apt install ./mysql-apt-config.deb
+sudo apt update
+sudo apt install mysql-workbench-community -y
+
